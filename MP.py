e5ee09a0ef8d1620c362b106c9947ef49fc558ec
redouane-maida <redouane.maida@laplateforme.io>
2024-10-23 10:23:02 +0200

    changement

--- MP.py
+++ MP.py
@@ -1,6 +1,21 @@
 """# Tic-Tac-Toe
 Matthieu, Franck, Redouane"""
 
-def print_board ( board ): 
-    pour la ligne dans le tableau : 
-        print ( "|" + "|" .join(row) + "|" )
+#Création de tableau et ses lignes.
+tableau = [" " for i in range(9)]
+def affichez_tableau():
+     Ligne1 = "| {} | {} | {} |".format(tableau[0], tableau[1], tableau[2])
+     Ligne2 = "| {} | {} | {} |".format(tableau[3], tableau[4], tableau[5])
+     Ligne3 = "| {} | {} | {} |".format(tableau[6], tableau[7], tableau[8])
+     print()
+     print(Ligne1)
+     print(Ligne2)
+     print(Ligne3)
+     print()
+
+def egalite():
+    if " " not in tableau:
+        return True
+    else:
+        return False
+    
