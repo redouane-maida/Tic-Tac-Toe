36b3ac17e066645e63d6002e475073bbbca9aaa4
redouane-maida <redouane.maida@laplateforme.io>
2024-10-23 14:42:26 +0200

    grand changement

--- MP.py
+++ MP.py
@@ -14,42 +14,6 @@ def affichez_tableau():
     print(Ligne3)
     print()
 
-def tour_joueur(icon):
-    if icon == "X":
-        nombre = 1
-    elif icon == "O":
-        nombre = 2
-    print("A ton tour joueur {}".format(nombre))
-    choix = int(input("Fais ton choix (1-9): ").strip())
-    if tableau[choix - 1] == " ":
-        tableau[choix - 1] = icon
-    else:
-        print("Cette case est déja prise!")
-        tour_joueur(icon)
-
-def victoire(icon):
-    if (tableau[0] == icon and tableau[1] == icon and tableau[2] == icon) or \
-       (tableau[3] == icon and tableau[4] == icon and tableau[5] == icon) or \
-       (tableau[6] == icon and tableau[7] == icon and tableau[8] == icon) or \
-       (tableau[0] == icon and tableau[3] == icon and tableau[6] == icon) or \
-       (tableau[1] == icon and tableau[4] == icon and tableau[7] == icon) or \
-       (tableau[2] == icon and tableau[5] == icon and tableau[8] == icon) or \
-       (tableau[0] == icon and tableau[4] == icon and tableau[8] == icon) or \
-       (tableau[2] == icon and tableau[4] == icon and tableau[6] == icon):
-        return True
-    else:
-        return False
-
-def egalite():
-    if " " not in tableau:
-        return True
-    else:
-        return False
-
-# Boucle principale du jeu
-while True:
-    affichez_tableau()
-    tour_joueur("X")
     
     if victoire("X"):
         affichez_tableau()
@@ -60,13 +24,4 @@ while True:
         print("C'est un match nul!")
         break
 
-    tour_joueur("O")
     
-    if victoire("O"):
-        affichez_tableau()
-        print("O gagne! Félicitation!")
-        break
-    elif egalite():
-        affichez_tableau()
-        print("C'est un match nul!")
-        break
