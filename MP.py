99761c4f18187ddf59539559d2a8249672300c90
redouane-maida <redouane.maida@laplateforme.io>
2024-10-23 14:06:05 +0200

    changement

--- MP.py
+++ MP.py
@@ -1,21 +1,72 @@
 """# Tic-Tac-Toe
 Matthieu, Franck, Redouane"""
 
-#Création de tableau et ses lignes.
+#Création du tableau et de ses lignes.
 tableau = [" " for i in range(9)]
+
 def affichez_tableau():
-     Ligne1 = "| {} | {} | {} |".format(tableau[0], tableau[1], tableau[2])
-     Ligne2 = "| {} | {} | {} |".format(tableau[3], tableau[4], tableau[5])
-     Ligne3 = "| {} | {} | {} |".format(tableau[6], tableau[7], tableau[8])
-     print()
-     print(Ligne1)
-     print(Ligne2)
-     print(Ligne3)
-     print()
+    Ligne1 = "| {} | {} | {} |".format(tableau[0], tableau[1], tableau[2])
+    Ligne2 = "| {} | {} | {} |".format(tableau[3], tableau[4], tableau[5])
+    Ligne3 = "| {} | {} | {} |".format(tableau[6], tableau[7], tableau[8])
+    print()
+    print(Ligne1)
+    print(Ligne2)
+    print(Ligne3)
+    print()
+
+def tour_joueur(icon):
+    if icon == "X":
+        nombre = 1
+    elif icon == "O":
+        nombre = 2
+    print("A ton tour joueur {}".format(nombre))
+    choix = int(input("Fais ton choix (1-9): ").strip())
+    if tableau[choix - 1] == " ":
+        tableau[choix - 1] = icon
+    else:
+        print("Cette case est déja prise!")
+        tour_joueur(icon)
+
+def victoire(icon):
+    if (tableau[0] == icon and tableau[1] == icon and tableau[2] == icon) or \
+       (tableau[3] == icon and tableau[4] == icon and tableau[5] == icon) or \
+       (tableau[6] == icon and tableau[7] == icon and tableau[8] == icon) or \
+       (tableau[0] == icon and tableau[3] == icon and tableau[6] == icon) or \
+       (tableau[1] == icon and tableau[4] == icon and tableau[7] == icon) or \
+       (tableau[2] == icon and tableau[5] == icon and tableau[8] == icon) or \
+       (tableau[0] == icon and tableau[4] == icon and tableau[8] == icon) or \
+       (tableau[2] == icon and tableau[4] == icon and tableau[6] == icon):
+        return True
+    else:
+        return False
 
 def egalite():
     if " " not in tableau:
         return True
     else:
         return False
+
+# Boucle principale du jeu
+while True:
+    affichez_tableau()
+    tour_joueur("X")
     
+    if victoire("X"):
+        affichez_tableau()
+        print("X gagne! Félicitation!")
+        break
+    elif egalite():
+        affichez_tableau()
+        print("C'est un match nul!")
+        break
+
+    tour_joueur("O")
+    
+    if victoire("O"):
+        affichez_tableau()
+        print("O gagne! Félicitation!")
+        break
+    elif egalite():
+        affichez_tableau()
+        print("C'est un match nul!")
+        break
